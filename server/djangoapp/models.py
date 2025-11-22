e56751fd2d03cb76c0bd173b24226acd0d2e61bc
SwarnaVahanan <swarna.alagu@gmail.com>
2025-11-22 10:19:04 +0300

    initial commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,7 +12,13 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country_of_origin = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +29,40 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('HATCHBACK', 'Hatchback'),
+        ('PICKUP', 'Pickup Truck'),
+        ('MINIVAN', 'Minivan'),
+        ('SPORTSCAR', 'Sports Car'),
+        ('CROSSOVER', 'Crossover'),
+        ('MUSCLE', 'Muscle Car'),
+        ('ELECTRIC', 'Electric'),
+        ('HYBRID', 'Hybrid'),
+    ]
+
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
+
+    year = models.IntegerField(
+        default=2025,
+        validators=[
+            MaxValueValidator(2025),
+            MinValueValidator(2015)
+        ]
+    )
+
+    engine_type = models.CharField(max_length=50, blank=True, null=True)
+    horsepower = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.car_make.name} {self.name}'
